f0129a4eb01a04a2b124b512c5a065f8e01a5fad
vance.petitt <vance.petitt@gmail.com>
2022-01-28 14:21:30 -0600

    added day_trip_generator fr storing trip variables, updated main.py

---
+++ day_trip_generator.py
@@ -0,0 +1,9 @@
+#lists
+from random import randrange,choice
+destination_list = ['the STL Arch', 'Forest Park', 'the Del Mar Loop', 'the STL Zoo', 'the City Museum']
+
+transportation_list = ['driving your car', 'taking the Metro', 'taking an Uber', 'bumming a ride from your friends']
+
+restaurant_list = ["Mama's on the Hill", 'Texas de Brazil', "Fitz's", 'La Catrina', 'The Shack']
+
+entertainment_list = ["going to a Blues game", "playing a round at Top Golf", 'visiting the Steinberg Skating Rink', 'taking a Budweiser Factory Tour']
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 #lists
 from random import randrange,choice
-destination_list = ['the STL Arch', 'Forest Park', 'the Del Mar Loop', 'the STL Zoo', 'the City Museum']
-
-transportation_list = ['driving your car', 'taking the Metro', 'taking an Uber', 'bumming a ride from your friends']
-
-restaurant_list = ["Mama's on the Hill", 'Texas de Brazil', "Fitz's", 'La Catrina', 'The Shack']
-
-entertainment_list = ["going to a Blues game", "playing a round at Top Golf", 'visiting the Steinberg Skating Rink', 'taking a Budweiser Factory Tour']
+from day_trip_generator import destination_list
+from day_trip_generator import transportation_list
+from day_trip_generator import restaurant_list
+from day_trip_generator import entertainment_list
 
 trip_components = [transportation_list, destination_list, restaurant_list, entertainment_list]
 
