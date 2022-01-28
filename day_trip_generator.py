9095e9d01a334ed75082ea32d6d64dfab7d8c74c
vance.petitt <vance.petitt@gmail.com>
2022-01-28 14:43:30 -0600

    Added more variety for the day trip options

--- day_trip_generator.py
+++ day_trip_generator.py
@@ -1,9 +1,9 @@
 #lists
 from random import randrange,choice
-destination_list = ['the STL Arch', 'Forest Park', 'the Del Mar Loop', 'the STL Zoo', 'the City Museum']
+destination_list = ['the St. Louis Arch', 'Forest Park', 'the Del Mar Loop', 'the St. Louis Zoo', 'the City Museum', "Grant's Farm", 'the Missouri Botanical Garden', 'Union Station', 'Six Flags', 'Meramac Caverns', 'Cahokia Mounds', 'Lone Elk Park']
 
-transportation_list = ['driving your car', 'taking the Metro', 'taking an Uber', 'bumming a ride from your friends']
+transportation_list = ['driving your car', 'taking the Metro', 'taking an Uber', 'bumming a ride from your friends', 'taking a Lyft', 'using a Lime scooter']
 
-restaurant_list = ["Mama's on the Hill", 'Texas de Brazil', "Fitz's", 'La Catrina', 'The Shack']
+restaurant_list = ["Mama's on the Hill", "Sugarfire BBQ",'Texas de Brazil', "Fitz's", 'La Catrina', 'The Shack', 'The Fountain on Locust', "Charlie Gitto's on the Hill", 'Blueberry Hill', 'Ted Drewes Frozen Custard', "Pappy's", 'Southwest Diner']
 
-entertainment_list = ["going to a Blues game", "playing a round at Top Golf", 'visiting the Steinberg Skating Rink', 'taking a Budweiser Factory Tour']
+entertainment_list = ["going to a Blues game", "playing a round at Top Golf", 'visiting the Steinberg Skating Rink', 'taking a Budweiser Factory Tour', 'going to a Cardinals game', 'visiting the bars in Soulard', "hearing live bands at Nick's Pub", "going to Ballpark Village"]
